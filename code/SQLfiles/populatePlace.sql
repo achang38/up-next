63b20e0dd842072fb5b3fed0812420e20501af51
jyfoster3 <jyfoster@wisc.edu>
2020-07-26 20:31:04 -0500

    place.csv added to cleans (still needs a little work for duplicates)

--- code/SQLfiles/populatePlace.sql
+++ code/SQLfiles/populatePlace.sql
@@ -3,20 +3,20 @@ use movieapp;
 -- Reformat Place to match the csv
 DROP TABLE Place;
 -- ALTER TABLE Place ADD COLUMN Country VARCHAR(2), ADD COLUMN City VARCHAR(255), ADD COLUMN AccentCity VARCHAR(255), ADD COLUMN Region VARCHAR(2), ADD COLUMN Latitude double, ADD COLUMN Longitude double, ADD PRIMARY KEY(City, Country, Region);
-create table Place(Country VARCHAR(2),
+create table Place(Country VARCHAR(255),
                    City VARCHAR(255),
                    AccentCity VARCHAR(255),
                    Region VARCHAR(2),
                    Population BIGINT,
-                   Latitude double,
-                   Longitude double,
-                   PRIMARY KEY(City, Country, Region)
+                   Latitude DOUBLE,
+                   Longitude DOUBLE,
+                   PRIMARY KEY(City, Country)
 );
 
 -- LOAD PLACE
-ALTER TABLE place ALTER Population SET DEFAULT 0;
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/worldcitiespopRef.csv' INTO TABLE place
-    FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
+-- ALTER TABLE place ALTER Population SET DEFAULT 0;
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/worldcitiesCleaned.csv' INTO TABLE place CHARACTER SET 'utf8'
+    FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
 
 
